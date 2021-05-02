adb97e257b4887478d84adeae782cc7974578a8f
Nanu Oghenetega <35012528+nanumichael27@users.noreply.github.com>
2021-05-02 22:25:57 +0100

    edited the payment controller to include an auth

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -9,6 +9,10 @@ use Illuminate\Support\Facades\Auth;
 
 class PaymentController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     //
     public function index(){
         return view('user.makepayment');
