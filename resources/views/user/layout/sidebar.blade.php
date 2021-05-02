6e14474d4abce4543a87d9060e5879539f1679a9
Nanu Oghenetega <35012528+nanumichael27@users.noreply.github.com>
2021-05-02 18:22:06 +0100

    updated navbar and sidebar colors

--- resources/views/user/layout/sidebar.blade.php
+++ resources/views/user/layout/sidebar.blade.php
@@ -1,8 +1,8 @@
   <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <aside class="main-sidebar sidebar-light-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{route('dashboard')}}" class="brand-link">
-      <img src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <a href="{{route('dashboard')}}" class="brand-link navbar-purple">
+      <img src="{{asset('images/logo-white.png')}}" alt="AdminLTE Logo" class="brand-image elevation-3" style="max-height: 27px;">
       <span class="brand-text font-weight-light">Data2Income</span>
     </a>
 
@@ -14,7 +14,7 @@
           <img src="{{ Storage::exists('profilepicture/'.Auth::user()->user_id.'.png') ? asset('profilepicture/'.Auth::user()->user_id.'.png?'.rand())  : asset('dist/img/user.svg') }}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">{{Auth()->user()->name}}</a>
+          <a href="{{route('userprofile')}}" class="d-block">{{Auth()->user()->name}}</a>
         </div>
       </div>
 
